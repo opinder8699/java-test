7ec8ba02c4d2fa8a16f47e1aada06685e121e88c
opinder8699 <opinder8699@gmail.com>
2026-06-27 17:52:30 +0530

    fourth

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Helloooooooo FORGECI");
+        System.out.println("Helloooooooo ForgeCI");
     }
 }
