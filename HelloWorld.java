c4fd19f4d11b023c9de225db182220dabfe9ffe3
opinder8699 <opinder8699@gmail.com>
2026-06-27 12:57:42 +0530

    second

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello ForgeCI!");
+        System.out.println("GURLEEN OPINDER VEERINDER");
     }
 }
